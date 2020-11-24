d5754442a1073cecb979e1abb53eb3cecf64f2fd
Chaitanya <chaitanyakarri963@gmail.com>
2020-11-24 17:33:49 +0530

    24-11-2020

---
+++ ExampleNav/app/src/main/java/com/example/chaitu/examplenav/HomeFragment.java
@@ -0,0 +1,44 @@
+package com.example.chaitu.examplenav;
+
+import android.media.tv.TvView;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class HomeFragment extends Fragment {
+    TextView red,green,blue;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        red = v.findViewById(R.id.red);
+        green = v.findViewById(R.id.green);
+        blue = v.findViewById(R.id.blue);
+        red.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_redFragment);
+            }
+        });
+        green.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_greenFragment);
+            }
+        });
+        blue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_homeFragment_to_blueFragment);
+            }
+        });
+        return v;
+    }
+}
