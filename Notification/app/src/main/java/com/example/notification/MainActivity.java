c21c5d9122325014c24d1e832857f10849de3912
varaprasad <varaprasad767>
2020-12-03 18:03:53 +0530

    notification

---
+++ Notification/app/src/main/java/com/example/notification/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.notification;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    NotificationManager manager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        manager= (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+    }
+
+    public void notification(View view) {
+        createNootification();
+        sendNotification();
+        
+    }
+
+    private void sendNotification() {
+        NotificationCompat.Builder builder= new NotificationCompat.Builder(this,
+                "Demo");
+        Intent i=new Intent(this,MainActivity.class);
+        PendingIntent pi= PendingIntent.getActivity(this,0,i,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentTitle("Notification");
+        builder.setContentText("This is my Notification");
+        builder.setSmallIcon(R.drawable.ic_launcher_background);
+        builder.setContentIntent(pi);
+        manager.notify(0,builder.build());
+
+
+    }
+
+    private void createNootification() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel= new NotificationChannel("Demo","vara",
+                    NotificationManager.IMPORTANCE_HIGH);
+            channel.enableVibration(true);
+            channel.enableLights(true);
+            channel.setLightColor(Color.RED);
+            manager.createNotificationChannel(channel);
+        }
+
+
+    }
+}
